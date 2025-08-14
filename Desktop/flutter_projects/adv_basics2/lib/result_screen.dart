bc1dd5f51fe2ed55d8394b21bdbc46d759236d29
shabzy <106165438+shabzzy@users.noreply.github.com>
2025-08-14 22:27:17 +0100

    second commit

--- Desktop/flutter_projects/adv_basics2/lib/result_screen.dart
+++ Desktop/flutter_projects/adv_basics2/lib/result_screen.dart
@@ -1,10 +1,56 @@
+import 'package:adv_basics2/data/question.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ResultScreen extends StatelessWidget {
-  const ResultScreen({super.key});
+  const ResultScreen(
+    this.restartQuiz, {
+    super.key,
+    required this.chosenAnswers,
+  }); //this.chosenAnsers is to access the selected answer form Quiz
+  final void Function() restartQuiz;
+  final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summaryData = [];
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summaryData.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+    return summaryData;
+  }
+
   @override
   Widget build(BuildContext context) {
-    
-    return Text('Welcome to the result screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text('YOu answered x out of Y correctly!'),
+            SizedBox(height: 30),
+            Text('List of answers and questions....'),
+            SizedBox(height: 30),
+            TextButton(
+              onPressed: restartQuiz,
+              child: Text(
+                'Restart Quiz!',
+                style: GoogleFonts.lato(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
